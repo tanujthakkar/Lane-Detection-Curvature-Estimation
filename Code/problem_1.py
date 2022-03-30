dc2bd5d7d368b26f132848682409e197f045894e
Tanuj Thakkar <tanujthakkar720@gmail.com>
2022-03-30 19:30:51 -0400

    Switched to HSV colorspace, started adaptive_equalize

--- Code/problem_1.py
+++ Code/problem_1.py
@@ -1,4 +1,15 @@
-#!/usr/bin/python3
+#!/usr/env/bin python3
+
+"""
+ENPM673 Spring 2022: Perception for Autonomous Robots
+
+Project 2 - Lane Detection & Curvature Estimation
+
+Author(s):
+Tanuj Thakkar (tanuj@umd.edu)
+M. Engg Robotics
+University of Maryland, College Park
+"""
 
 # Importing modules
 import os
@@ -6,14 +17,15 @@ import sys
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import argparse
 
 from utils import *
 
 
 class HistogramEqulization:
 
-    def __init__(self, img_path: str) -> None:
-        self.img = cv2.imread(img_path)
+    def __init__(self, img: np.array) -> None:
+        self.img = img
         self.img_gray = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
         self.output = None
 
@@ -63,13 +75,18 @@ class HistogramEqulization:
 
         return cdf
 
-    def equalize(self) -> np.array:
+    def equalize(self, img: np.array) -> np.array:
 
-        b, g, r = cv2.split(self.img)
-        img_split = [b, g, r]
-        self.output = list()
+        # b, g, r = cv2.split(img)
+        # img_split = [b, g, r]
 
-        for i, c in enumerate(img_split):
+        lab_img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        h, s, v = cv2.split(img)
+        img_split = [h, s, v]
+
+        output = list()
+
+        for i, c in enumerate(img_split[:1]):
             histogram = self.__compute_histogram(c)
             cdf = self.__compute_cdf(histogram)
             cdf_min = np.min(cdf[np.nonzero(cdf)])
@@ -82,24 +99,51 @@ class HistogramEqulization:
                 channel[i] = ((cdf[pixel] - cdf_min)/((h*w) - cdf_min)) * 255
 
             channel = channel.reshape(c.shape)
-            self.output.append(channel)
+            output.append(channel)
 
-        self.output = cv2.merge((self.output[0], self.output[1], self.output[2]))
+        output = np.dstack((channel, s, v))
 
-        return self.output
+        # self.output = output
+        # cv2.imshow("Result", np.uint8(output))
+        # cv2.waitKey()
+
+        return output
+
+    def adaptive_equalize(self) -> np.array:
+
+        self.output = np.copy(self.img)
+
+        r_step = self.img.shape[0]//8
+        c_step = self.img.shape[1]//8
+
+        for r in range(0, self.img.shape[0], r_step):
+            for c in range(0, self.img.shape[1], c_step):
+                self.output[r:r+r_step, c:c+c_step, :] = self.equalize(self.img[r:r+r_step, c:c+c_step, :])
 
     def visualize(self) -> None:
         cv2.imshow("Image", self.img)
-        cv2.imshow("Image Grayscale", self.img_gray)
         cv2.imshow("Result", np.uint8(self.output))
         cv2.waitKey()
 
 
-def main():
+def hist_equalize_img(img_path: str) -> np.array:
 
-    HE = HistogramEqulization('../Data/adaptive_hist_data/0000000024.png')
-    HE.equalize()
+    img = cv2.imread(img_path)
+    HE = HistogramEqulization(img)
+    # HE.equalize(img)
+    HE.adaptive_equalize()
     HE.visualize()
+
+
+def main():
+    Parser = argparse.ArgumentParser()
+    Parser.add_argument('--ImagePath', type=str, default="../Data/adaptive_hist_data/0000000024.png", help='Path to input image')
+    Parser.add_argument('--Visualize', action='store_true', help='Toggle visualization')
+    
+    Args = Parser.parse_args()
+    image_path = Args.ImagePath
+
+    hist_equalize_img(image_path)    
 
 
 if __name__ == '__main__':
